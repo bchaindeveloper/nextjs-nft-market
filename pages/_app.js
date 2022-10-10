25c5cf34660940e395b47c8524d5ff1192752479
bchaindeveloper <bcdev3@protonmail.com>
2022-10-10 12:24:19 -0400

    Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import Header from "../components/Header"
+import { MoralisProvider } from "react-moralis"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+
+            <MoralisProvider initializeOnMount={false}>
+                    <Header />
+                    <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
