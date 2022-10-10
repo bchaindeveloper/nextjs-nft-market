da566de384e82e8c0d14e0549faa18f68e292a18
bchaindeveloper <bcdev3@protonmail.com>
2022-10-10 17:19:00 -0400

    Add Events

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import "../styles/globals.css"
 import Header from "../components/Header"
 import { MoralisProvider } from "react-moralis"
+import Head from 'next/head'
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
 
 function MyApp({ Component, pageProps }) {
     return (
         <div>
-
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta name="description" content="NFT Marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
             <MoralisProvider initializeOnMount={false}>
                     <Header />
                     <Component {...pageProps} />
