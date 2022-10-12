5640769be1625373df041419542b05bb7669e299
bchaindeveloper <bcdev3@protonmail.com>
2022-10-12 12:53:09 -0400

    index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {useMoralisQuery} from "react-moralis"
+import { privateDecrypt } from 'crypto'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      Hi
-      </div>
-      )
+  const {data: listedNfts, isFetching: fetchingListedNfts}
+ = useMoralisQuery(
+  "ActiveItem",
+  (query) => query.limit(10).descending("tokenId")
+ )  
+ console.log(listedNfts)
+
+return (<div className={styles.contrainer}>
+  {fetchingListedNfts ? (<div>Loading...</div>) : listedNfts.map(nft => {
+    console.log(nft.attributes)
+    const {price, nftAddress, tokenId, marketplaceAddress, seller} = nft.attributes
+    return(
+      <div>Price: {price}. nftAddress: {nftAddress}. TokenId: {tokenId}. Seller: {seller}</div>
+    )
+  })}
+
+</div>)
 }
+ 
